7f773d17a06784e8fe1557a9f007405f375492a4
marquisalexander <marquissantos123@gmail.com>
2021-04-05 06:44:23 -0300

    create pag posts

--- src/pages/api/webhooks.ts
+++ src/pages/api/webhooks.ts
@@ -24,7 +24,10 @@ export const config = {
 }
 
 const relevantEvents = new Set([
-    'checkout.session.completed'
+    'checkout.session.completed',
+    // 'customer.subscription.created',
+    'customer.subscription.updated',
+    'customer.subscription.deleted',
 ])
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
@@ -45,13 +48,28 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         if (relevantEvents.has(type)) {
             try {
                 switch (type) {
+                    // case 'customer.subscription.created':
+                    case 'customer.subscription.updated':
+                    case 'customer.subscription.deleted':
+
+                    const subscription = event.data.object as Stripe.Subscription;
+
+                    await saveSubscription(
+                        subscription.id,
+                        subscription.customer.toString(),
+                        // type === 'customer.subscription.created',
+                        false
+                    );
+
+                        break;
                     case 'checkout.session.completed':
 
                     const checkoutSession = event.data.object as Stripe.Checkout.Session
 
                         await saveSubscription(
                             checkoutSession.subscription.toString(),
-                            checkoutSession.customer.toString()
+                            checkoutSession.customer.toString(),
+                            true
                         )
 
                         break;
